024d2fcb202a8f15acc9b15f15e48b2ee84040ec
danielforkner <daniel.forkner@gmail.com>
2022-03-27 19:18:26 -0400

    added cat, ls, and pwd functions

--- bash.js
+++ bash.js
@@ -1,11 +1,26 @@
+const pwd = require('./pwd.js');
+const ls = require('./ls.js');
+const cat = require('./cat.js');
+
 process.stdout.write('prompt $ ');
 
 process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();
+  const cmd = data.toString().trim().split(' ')[0];
+  const arg = data.toString().trim().split(' ')[1];
   switch (cmd) {
     case 'pwd':
-      process.stdout.write(process.cwd());
-      process.stdout.write('\nprompt $ ');
+      pwd();
+      break;
+    case 'ls':
+      ls();
+      break;
+    case 'cat':
+      if (!arg) {
+        process.stdout.write('You must specify a file path');
+        process.stdout.write('\nprompt $ ');
+        break;
+      }
+      cat(arg);
       break;
     default:
       process.stdout.write('You typed: ' + cmd);
